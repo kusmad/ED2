06e0c8ff5631683fc789279243efb3f7f70b1b8a
kusmad <danielschaefferkusma@gmail.com>
2018-11-22 18:50:25 -0200

    Add files via upload
    Finalizada a função de vértice final.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,24 @@
 #include<fstream>
 
 //ajuste: calcular o grau do vertice logo após a leitura para facilitar nas outras duas fuções;
+//ajuste2: na função do grau um dos for's era desnecessário e tava bugando o V05;
 
 using namespace std;
+
+int menu(){
+    cout << "Digite a opcao desejada:\n\n";
+    cout << "1- Arvore Geradora Minima\n";
+    cout << "2- Grau vertice\n";
+    cout << "3- Vertices finais\n";
+    cout << "4- incidencia\n";
+    cout << "5- Circuito\n";
+    cout << "6- Sequencia de graus\n";
+    cout << "7- Sair\n";
+    int op;
+    cin >> op;
+    return op;
+
+}
 
 struct dados{
     int numero_v; // número do vértice
@@ -17,21 +33,10 @@ struct dados{
     int grau;
 };
 
-void menu(){
-    cout << "Digite a opcao desejada:\n\n";
-    cout << "1- Arvore Geradora Minima\n";
-    cout << "2- Grau vertice\n";
-    cout << "3- Vertices finais\n";
-    cout << "4- incidencia\n";
-    cout << "5- Circuito\n";
-    cout << "6- Sequencia de graus\n";
-    cout << "7- Fim\n";
-}
-
 bool leitura_arq(list<dados> &grafo){
     ifstream arq;
-    dados leitura, p;
-    int cont;
+    dados aux;
+    int qtd;
 
     arq.open("grafo.txt");
 
@@ -40,64 +45,88 @@ bool leitura_arq(list<dados> &grafo){
         return 0;
     }
 
-    while(arq >> leitura.numero_v){
-        cont = leitura.numero_v;
-        for(int i=0; i<cont; i++){
-            arq >> leitura.rotulo;
-            grafo.push_back(leitura);
-    }
-        arq >> leitura.numero_aresta;
-        cont = leitura.numero_aresta;
-        for(int j=0; j<cont; j++){
-            arq >> leitura.rotulo_aresta >> leitura.v_inicial >> leitura.v_final >> leitura.custo;
-            grafo.push_back(leitura);
+    if(arq >> aux.numero_v){
+        qtd = aux.numero_v;
+        for(int i=0; i<qtd; i++){
+            arq >> aux.rotulo;
+            grafo.push_back(aux);
         }
     }
-    grafo.push_back(leitura);
 
+    if(arq >> aux.numero_aresta){
+        qtd = aux.numero_aresta;
+            for(int j=0; j<qtd; j++){
+                arq >> aux.rotulo_aresta >> aux.v_inicial >> aux.v_final >> aux.custo;
+                grafo.push_back(aux);
+            }
+    }
 
     return 0;
-
 }
 
-bool grau_vertice(list<dados> &grafo){
+bool grau(list<dados> grafo){
     string vertice;
-    int acum =0;
     cout << "Digite o nome do vertice desejado: ";
     cin >> vertice;
+    int acum =0;
 
     for(auto it=grafo.begin(); it!=grafo.end(); it++){
-        if(vertice == it->v_inicial || vertice == it->v_final) acum ++;
+        if(vertice == it->v_inicial || vertice == it->v_final){
+            acum++;
+        }
     }
-        cout << "Grau do vertice " << vertice << ":" << acum-1 << endl;
+    if(acum!=0)
+        cout << "grau de " << vertice << ": " << acum << endl;
+    else
+        cout << "grau de " << vertice << ": " << "Nao existe." << endl;
+
+    cout << endl;
     return 0;
 }
 
-bool vertice_final(list<dados> grafo){
-    int total_grau;
-    dados insere_grau;
-    string rot;
-    auto j = grafo.begin();
+bool incidente(list<dados> grafo){
+    dados aux;
+    cout << "Digite o nome da aresta desejada: ";
+    cin >> aux.rotulo_aresta;
 
-    int p = 0;
-    while(p<j->numero_v){
-        rot = j->rotulo;
-        total_grau = 0;
+    for(auto it=grafo.begin(); it!=grafo.end(); it++){
+        if(aux.rotulo_aresta == it->rotulo_aresta){
+
+        }
+    }
+
+    return 0;
+}
+
+bool vertice_final(list<dados> &grafo){
+    int cont, vf;
+    list<dados>::iterator p=grafo.begin();
+    string v;
+    cont = p->numero_v;
+   // cout << p->numero_v << endl;
+    //cout << cont << endl;
+    while(cont--){
+        v = p->rotulo;
+      //  cout << v << endl;
+        vf = 0;
         for(auto it=grafo.begin(); it!=grafo.end(); it++){
-            if(rot == it->v_inicial || rot == it->v_final) total_grau ++;
+            if(v == it->v_inicial || v == it->v_final){
+                vf ++;
+               // cout << vf << endl;
+            }
+        }
+        if(vf==1){
+            cout << "Vertice final: " << v << endl;
+
+        }
+        else{
+            cout << "Nao ha vertices finais no grafo\n\n";
         }
         p++;
-        j++;
-        if(total_grau == 1)
-            cout << "Vertice(s) final(is): " << rot << endl;
-
-        else{
-            cout << "Nao foram encontrados vertices finais\n\n";
-            return 0;
-        }
-
     }
 }
+
+
 
 int main(){
 
@@ -106,23 +135,24 @@ int main(){
 
     leitura_arq(grafo);
 
-    menu();
-
-    cin >> op;
-
     do{
+        op = menu();
         switch(op){
             case 2:
-                grau_vertice(grafo);
+                grau(grafo);
                 break;
             case 3:
                 vertice_final(grafo);
                 break;
+            case 4:
+                incidente(grafo);
+                break;
 
-        }
-
+            default:
+        cout<<"Opcao invalida";
+        break;
+    }
     }while(op!=7);
 
     return 0;
-
 }
